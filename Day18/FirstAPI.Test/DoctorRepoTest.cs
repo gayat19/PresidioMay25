b02ddb25e0f4c5eae684f40906f48be3e555c342
Gayathri Mahadevan <gayat_19@yahoo.com>
2025-06-03 13:34:11 +0530

    Exception Test

--- Day18/FirstAPI.Test/DoctorRepoTest.cs
+++ Day18/FirstAPI.Test/DoctorRepoTest.cs
@@ -20,7 +20,7 @@ public class Tests
         _context = new ClinicContext(options);
     }
 
-    [Test]
+    /*[Test]
     public async Task AddDoctorTest()
     {
         //arrange
@@ -47,9 +47,76 @@ public class Tests
         var result = await _doctorRepository.Add(doctor);
         //assert
         Assert.That(result, Is.Not.Null, "Doctor IS not addeed");
-        Assert.That(result.Id, Is.EqualTo(2));
+        Assert.That(result.Id, Is.EqualTo(1));
     }
+    [TestCase(1)]
+    [TestCase(2)]
+    public async Task GetDoctorPassTest(int id)
+    {
+        //arrange
+        var email = " test@gmail.com";
+        var password = System.Text.Encoding.UTF8.GetBytes("test123");
+        var key = Guid.NewGuid().ToByteArray();
+        var user = new User
+        {
+            Username = email,
+            Password = password,
+            HashKey = key,
+            Role = "Doctor"
+        };
+        _context.Add(user);
+        await _context.SaveChangesAsync();
+        var doctor = new Doctor
+        {
+            Name = "test",
+            YearsOfExperience = 2,
+            Email = email
+        };
+        IRepository<int, Doctor> _doctorRepository = new DoctorRepository(_context);
+        //action
+        await _doctorRepository.Add(doctor);
 
+        //action
+        var result = _doctorRepository.Get(id);
+        //assert
+        Assert.That(result.Id, Is.EqualTo(id));
+
+    }*/
+
+    [TestCase(3)]
+    public async Task GetDoctorExceptionTest(int id)
+    {
+        //arrange
+        var email = " test@gmail.com";
+        var password = System.Text.Encoding.UTF8.GetBytes("test123");
+        var key = Guid.NewGuid().ToByteArray();
+        var user = new User
+        {
+            Username = email,
+            Password = password,
+            HashKey = key,
+            Role = "Doctor"
+        };
+        _context.Add(user);
+        await _context.SaveChangesAsync();
+        var doctor = new Doctor
+        {
+            Name = "test",
+            YearsOfExperience = 2,
+            Email = email
+        };
+        IRepository<int, Doctor> _doctorRepository = new DoctorRepository(_context);
+        //action
+        await _doctorRepository.Add(doctor);
+        //action
+
+        var ex =await  Assert.ThrowsAsync<Exception>(() =>  _doctorRepository.Get(id));
+        //assert
+        //Assert.That(() => _doctorRepository.Get(id), Throws.TypeOf<Exception>());
+        Assert.That(ex.Message,Is.EqualTo("No doctor with teh given ID"));
+       
+
+    }
     [TearDown]
     public void TearDown() 
     {
